22d07cab60ea877a37f614b61825672967574742
radu781 <poparadu501@gmail.com>
2021-10-22 13:04:38 +0300

    Added logger header
    Logger functions help with debug and Client-Server communications

---
+++ Client/include/logger.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#ifdef LOGGER_LEVEL
+#define LOG_ALL 2
+#define LOG_SOME 1
+#define LOG_NONE 0
+#endif
+
+FILE *_logger;
+bool _fileExists = false;
+
+/**
+ * Function that logs messages based on a predefined value given as an argument
+ * with the '-D' gcc flag, where:
+ * LOGGER_LEVEL == 2: all debug and communication info is logged
+ * LOGGER_LEVEL == 1: all Client/Server communication info is logged
+ * LOGGER_LEVEL == 0: nothing is logged
+ */
+void logDebug(const char *format, ...)
+{
+#if (!defined(LOGGER_LEVEL) || LOGGER_LEVEL < LOG_ALL)
+    return;
+#elif LOGGER_LEVEL == LOG_ALL
+    va_list args;
+    va_start(args, format);
+    va_end(args);
+
+    if (!_fileExists)
+    {
+        _logger = fopen("log.txt", "w");
+        _fileExists = true;
+    }
+    fprintf(_logger, "[DEBUG] ");
+    vfprintf(_logger, format, args);
+    fprintf(_logger, "\n");
+    fflush(_logger);
+#endif
+}
+
+void logComm(FILE *where, const char *format, ...)
+{
+#if (!defined(LOGGER_LEVEL) || LOGGER_LEVEL == LOG_NONE)
+    return;
+#elif LOGGER_LEVEL >= LOG_SOME
+    va_list args;
+    va_start(args, format);
+    va_end(args);
+
+    if (!_fileExists)
+    {
+        _logger = fopen("log.txt", "w");
+        _fileExists = true;
+    }
+    fprintf(_logger, "[COMM] ");
+    vfprintf(_logger, format, args);
+    fprintf(_logger, "\n");
+    fflush(_logger);
+
+    fprintf(where, "[COMM] ");
+    vfprintf(where, format, args);
+    fprintf(where, "\n");
+    fflush(where);
+#endif
+}
+
+void logCleanUp()
+{
+    if (_fileExists)
+    {
+        fclose(_logger);
+        _fileExists = false;
+    }
+}
