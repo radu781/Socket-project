c2a2bbd8fa8b282da3791fab65fb6f7051d4f2a0
radu781 <poparadu501@gmail.com>
2021-10-25 11:31:44 +0300

    Fully implemented login and help commands

--- Server/include/commands.h
+++ Server/include/commands.h
@@ -1,11 +1,14 @@
 #pragma once
 
+#include <stdio.h>
 #include "user_info.h"
 #include "../../shared/include/logger.h"
 
-const size_t commandCount = 6;
-const char supportedCommands[6][17] = {
+#define _COMMAND_COUNT 6
+
+const char supportedCommands[_COMMAND_COUNT][17] = {
     "login", "get-logged-users", "get-proc-info", "logout", "quit", "help"};
+
 typedef struct Command
 {
     char *body;
@@ -13,24 +16,43 @@ typedef struct Command
     unsigned argCount;
 };
 
+bool _findUser(const char *username)
+{
+    FILE *_configFile = fopen(".config", "r");
+    char buff[_USERLEN];
+
+    while (fscanf(_configFile, "%s", buff) != EOF)
+        if (strcmp(buff, username) == 0)
+        {
+            fclose(_configFile);
+            return true;
+        }
+
+    fclose(_configFile);
+    return false;
+}
+
 char *_login(const struct Command *cmd)
 {
+    if (cmd->argCount == 0)
+        return "Usage: login <user>";
+
     if (_loggedIn)
     {
-        char *buff = (char *)allocatePtr(sizeof(char), 43 + 10);
-        snprintf(buff, 43 + 10, "Already logged in as %s, try \"logout\" first", _username);
+        char *buff = (char *)allocatePtr(sizeof(char), 47 + _USERLEN);
+        snprintf(buff, 47 + _USERLEN, "Already logged in as %s, try \"logout\" first", _username);
         _haveAllocated = true;
         return buff;
     }
-    if (true)
+    if (_findUser(cmd->args[0]))
     {
+        strcpy(_username, cmd->args[0]);
         logComm(stdout, "Logged in as %s", _username);
         _loggedIn = true;
         return "";
     }
 
-    _haveAllocated = false;
-    return "Unexpected error";
+    return "User not found";
 }
 
 char *_getUsers(const struct Command *cmd)
@@ -45,7 +67,12 @@ char *_getInfo(const struct Command *cmd)
 
 char *_logout(const struct Command *cmd)
 {
-    return "";
+    if (!_loggedIn)
+        return "Already logged out";
+    
+    _loggedIn = false;
+    strcpy(_username, "");
+    return "Logged out";
 }
 
 char *_quit(const struct Command *cmd)
@@ -55,5 +82,11 @@ char *_quit(const struct Command *cmd)
 
 char *_help(const struct Command *cmd)
 {
-    return "";
+    return "Command list:\n\
+\tlogin <user> (logs you in as <user> if they exist)\n\
+\tget-logged-users (get the users on the system)\n\
+\tget-proc-info <pid> (get information about the process with pid <pid>)\n\
+\tlogout (logs you out of the current user)\n\
+\tquit (?)\n\
+\thelp (displays this)";
 }
