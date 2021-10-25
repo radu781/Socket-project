f27fbffb27bc7602f281e7ff642f16abb9395887
radu781 <poparadu501@gmail.com>
2021-10-25 14:25:05 +0300

    Fully implemented get-proc-info command

--- Server/include/commands.h
+++ Server/include/commands.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include "user_info.h"
 #include "../../shared/include/logger.h"
+#include "../../shared/include/memory.h"
 
 #define _COMMAND_COUNT 6
 
@@ -62,14 +65,36 @@ char *_getUsers(const struct Command *cmd)
 
 char *_getInfo(const struct Command *cmd)
 {
-    return "";
+    if (cmd->argCount == 0)
+        return "Usage: get-proc-info <pid>";
+    if (!_loggedIn)
+        return "This command is available only to logged users, try \"login <user>\"";
+
+    char path[24];
+    snprintf(path, 24, "/proc/%s/status", cmd->args[0]);
+    if (access(path, F_OK))
+        return "Process not found";
+    if (access(path, R_OK))
+        return "Unable to read";
+    FILE *statusFile = fopen(path, "r");
+
+    char *buff = NULL;
+    size_t _buffSize;
+    char *out = (char *)allocatePtr(sizeof(char), 128);
+    _haveAllocated = true;
+    while (getline(&buff, &_buffSize, statusFile) != EOF)
+        if (strstr(buff, "Name") || strstr(buff, "State") || (strstr(buff, "Pid") && !strstr(buff, "TracerPid")) ||
+            strstr(buff, "Uid") || strstr(buff, "VmSize"))
+            strcat(out, buff);
+
+    return out;
 }
 
 char *_logout(const struct Command *cmd)
 {
     if (!_loggedIn)
         return "Already logged out";
-    
+
     _loggedIn = false;
     strcpy(_username, "");
     return "Logged out";
