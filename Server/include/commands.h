058cdd7c77ef3a0a2311f6cb482b3a496e54a659
radu781 <poparadu501@gmail.com>
2021-10-26 12:12:56 +0300

    Added permission checks for config file

--- Server/include/commands.h
+++ Server/include/commands.h
@@ -7,11 +7,13 @@
 #include "user_info.h"
 #include "../../shared/include/logger.h"
 #include "../../shared/include/memory.h"
+#include "../../shared/include/constants.h"
 
 #define _COMMAND_COUNT 7
 
 const char supportedCommands[_COMMAND_COUNT][17] = {
     "login", "register", "get-logged-users", "get-proc-info", "logout", "quit", "help"};
+const char* _configFile = ".config";
 
 bool _hasQuit = false;
 
@@ -24,17 +26,28 @@ typedef struct Command
 
 bool _findUser(const char *username)
 {
-    FILE *_configFile = fopen(".config", "r");
+    FILE *config = fopen(_configFile, "r");
+    if (access(_configFile, F_OK))
+    {
+        logDebug("Can't create config file %s", _configFile);
+        return false;
+    }
+    if (access(_configFile, R_OK))
+    {
+        logDebug("Can't read config file %s", _configFile);
+        return false;
+    }
+
     char buff[_USERLEN];
 
-    while (fscanf(_configFile, "%s", buff) != EOF)
+    while (fscanf(config, "%s", buff) != EOF)
         if (strcmp(buff, username) == 0)
         {
-            fclose(_configFile);
+            fclose(config);
             return true;
         }
 
-    fclose(_configFile);
+    fclose(config);
     return false;
 }
 
@@ -75,12 +88,23 @@ char *_register(const struct Command *cmd)
     }
     if (!_findUser(cmd->args[0]))
     {
-        FILE *_configFile = fopen(".config", "a");
-        fprintf(_configFile, "%s\n", cmd->args[0]);
+        FILE *config = fopen(_configFile, "a");
+        if (access(_configFile, F_OK))
+        {
+            logDebug("Can't open config file %s", _configFile);
+            return "Internal error";
+        }
+        if (access(_configFile, W_OK))
+        {
+            logDebug("Can't write to config file %s", _configFile);
+            return "Internal error";
+        }
+
+        fprintf(config, "%s\n", cmd->args[0]);
         strcpy(_username, cmd->args[0]);
         logComm(stdout, "Registered new user: %s", _username);
         _loggedIn = true;
-        fclose(_configFile);
+        fclose(config);
         return "";
     }
 
@@ -148,7 +172,7 @@ char *_logout(const struct Command *cmd)
 
 char *_quit(const struct Command *cmd)
 {
-    char *buff = (char*)allocatePtr(sizeof(char), 5 + strlen(padding));
+    char *buff = (char *)allocatePtr(sizeof(char), 5 + strlen(padding));
     snprintf(buff, 5 + strlen(padding), "quit%s", padding);
     _haveAllocated = true;
     _hasQuit = true;
